c9e732becedddc94c85a44e0f66d88cc22aea52a
Ashwin Pillay <ashwin240298@gmail.com>
2021-05-28 22:18:17 +0530

    TEST-5: Replaced Entrada with World
    Demonstration for Internal demo

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println(new App().greet("Entrada!"));
+        System.out.println(new App().greet("World!"));
     }
 
     public String greet(String name) {
