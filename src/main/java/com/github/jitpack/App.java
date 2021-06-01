d2d2f34556f6988ed8ece8e172ddc0c1f2fc0089
Ashwin Pillay <ashwin240298@gmail.com>
2021-06-01 18:39:51 +0530

    TEST-6: Changed All to Al
    sdf

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println(new App().greet("All!"))
+        System.out.println(new App().greet("Al!"))
     }
 
     public String greet(String name) {
