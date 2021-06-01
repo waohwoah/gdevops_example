cbb7df4702deeebe36fa48e2b20f6c61a7e6cc3c
Ashwin Pillay <ashwin240298@gmail.com>
2021-06-01 18:33:39 +0530

    TEST-6: Error induced
    test

--- src/main/java/com/github/jitpack/App.java
+++ src/main/java/com/github/jitpack/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main(String[] args)
     {
-        System.out.println(new App().greet("All!"));
+        System.out.println(new App().greet("All!"))
     }
 
     public String greet(String name) {
